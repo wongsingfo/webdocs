eec8f33d08866cfeeefa9c85cc79847df2d7c75c
Chengke Wang <wongck@pku.edu.cn>
2020-08-13 21:19:00 +0800

    add: tutorial 3: Views and templates

--- edit/views.py
+++ edit/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Document
+
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Here is edit view")
+    latest_documents = Document.objects.order_by('-last_modified')[:5]
+    template = loader.get_template('edit/index.html')
+    context = {
+        'latest_documents': latest_documents
+    }
+    output = template.render(context, request)
+    # or shortcut reader(request, 'xxx.html', {context ...} )
+    return HttpResponse(output)
+
+
+def markdown_edit(request, document_id):
+    # or shortcut: get_object_or_404(Document, pk=document_id)
+    try:
+        document = Document.objects.get(pk=document_id)
+    except Document.DoesNotExist:
+        raise Http404("Document %s does not exist" % document_id)
+
+    return render(request, "edit/markdown_edit.html", {
+        'document': document
+    })
