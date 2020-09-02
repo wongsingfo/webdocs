33ad01e6bdacbebb76e63477508f12e60437c3cc
Jahoo <jahoo@pku.edu.cn>
2020-09-02 21:05:48 +0800

    chore: add vue.config.js

---
+++ webdocs_frontend/vue.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    devServer: {
+      // Server: 'http://10.128.202.42:8000'
+      // LocalServer: 'http://localhost:8000'
+      proxy: {
+        '^/api/': {
+          target: 'http://localhost:8000'
+        },
+        '^/admin/': {
+          target: 'http://localhost:8000'
+        },
+        '^/static/rest_framework/': {
+          target: 'http://localhost:8000'
+        }
+      }
+    },
+
+    chainWebpack: config => {
+      config
+        .plugin('html')
+        .tap(options => {
+          if (options[0].minify) {
+            options[0].minify.minifyCSS = true
+          }
+          return options
+        })
+
+      // config.module
+      //   .rule('gzip')
+      //   .test(/\.gz$/)
+      //   .use('gzip-loader')
+      //   .loader('gzip-loader')
+      //   .end()
+
+      // config.module
+      //   .rule('txt')
+      //   .test(/\.txt$/)
+      //   .use('@/loaders/txtLoader')
+      //   .loader('@/loaders/txtLoader')
+      //   .end()
+    },
+
+    productionSourceMap: false,
+    assetsDir: 'static'
+  }
