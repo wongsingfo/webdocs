8ddddd2b7da9142879c0c48a9aa9dbe65ad3d675
Chengke Wang <wongck@pku.edu.cn>
2020-09-05 17:12:58 +0800

    fix: Don't show bodies when retrieving multiple documents

--- page/serializers.py
+++ page/serializers.py
@@ -3,13 +3,32 @@ from rest_framework import serializers
 
 from page.models import Document, Image
 
-# TODO: 分两种序列化，列表中不需要包含body等字段
+
 class DocumentSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Document
         fields = ['url', 'id', 'title', 'owner', 'body', 'created', 'last_modified']
+
+    @classmethod
+    def many_init(cls, *args, **kwargs):
+        allow_empty = kwargs.pop('allow_empty', None)
+        child_serializer = cls(*args, **kwargs)
+        # Don't show body when retrieving multiple documents
+        child_serializer.fields.pop('body')
+        list_kwargs = {
+            'child': child_serializer,
+        }
+        if allow_empty is not None:
+            list_kwargs['allow_empty'] = allow_empty
+        list_kwargs.update({
+            key: value for key, value in kwargs.items()
+            if key in serializers.LIST_SERIALIZER_KWARGS
+        })
+        meta = getattr(cls, 'Meta', None)
+        list_serializer_class = getattr(meta, 'list_serializer_class', serializers.ListSerializer)
+        return list_serializer_class(*args, **list_kwargs)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
